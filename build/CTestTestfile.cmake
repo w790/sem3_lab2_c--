9f1a85093fdb9857e2571ffeaea4f6a30f1a08c7
Pavel <pasha_w790@bk.ru>
2025-10-07 22:03:50 +0700

    testttt

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/study/C++SEM3/lab2
+# Build directory: C:/study/C++SEM3/lab2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Lab2Tests "C:/study/C++SEM3/lab2/build/lab2_tests.exe")
+set_tests_properties(Lab2Tests PROPERTIES  _BACKTRACE_TRIPLES "C:/study/C++SEM3/lab2/CMakeLists.txt;27;add_test;C:/study/C++SEM3/lab2/CMakeLists.txt;0;")
